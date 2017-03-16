dc10b8f8aab306e57fb3fb2f4375e8015138407d
Bill Mitchell <wkmitch@gmail.com>
2017-03-15 18:47:15 -0700

    After lesson 02

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/threads', 'ThreadsController@index');
+Route::get('/threads/{thread}', 'ThreadsController@show', 'id');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
